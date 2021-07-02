6a326505e9163c9af5020725c822c9c6fde2d325
momotofff <momotofff@mail.ru>
2021-07-02 17:16:29 +0400

    added buttons and functionality

--- app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -19,18 +19,35 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         window = findViewById(R.id.window);
+        window.setText("0");
+        List<Integer> list = new ArrayList<>();
     }
 
     public void onClickButtonNumber(View v)
     {
-        window.setText(window.getText().toString() + ((Button)v).getText().toString());
+        String str = window.getText().toString();
+        if(str.equals("0"))
+            window.setText(((Button)v).getText().toString());
+        else
+            window.setText(window.getText().toString() + ((Button)v).getText().toString());
     }
 
     public void onClickButtonResult(View v)
     {
-        List<Integer> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        char[] allValues = (window.getText().toString().toCharArray());
+        String oneValue;
+        String twoValue;
 
+        for (int i = 0; i < allValues.length; ++i)
+        {
 
-        window.setText(window.getText().toString() + ((Button)v).getText().toString());
+        }
+        //window.setText(window.getText().toString() + ((Button)v).getText().toString());
+    }
+
+    public void onClickButtonDelete(View v)
+    {
+        window.setText("0");
     }
 }
